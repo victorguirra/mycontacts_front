7e923ad0178ab90d9e4371af6b65c5a6f12d32ba
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-31 17:18:39 -0300

    create select and input components

---
+++ src/components/Select.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export default styled.select`
+	width: 100%;
+	height: 52px;
+
+	padding:0 16px;
+
+	background: #FFF;
+	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+
+	border: 2px solid transparent;
+	border-radius: 4px;
+
+	font-size: 16px;
+
+	outline: none;
+	transition:border-color .2s ease-in;
+
+	&:focus {
+		border: 2px solid ${({ theme }) => theme.colors.primary.main};
+	}
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Faculdade</option>
+        <option value="123">Discord</option>
+      </Select>
+    </>
   );
 }
