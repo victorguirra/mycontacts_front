824053c70408ff0aa30c744f08b86bf470a87031
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-31 16:45:59 -0300

    create page header component

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,5 +1,7 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
   return (
-    <h1>EditContact Page</h1>
+    <PageHeader title="Editar Victor Guirra" />
   );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,7 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
   return (
-    <h1>NewContact Page</h1>
+    <PageHeader title="Novo Contato" />
   );
 }
