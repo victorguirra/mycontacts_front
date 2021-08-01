e253b80bc4424e784de612ff880b4c93872ac494
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-01 20:40:56 -0300

    update input box-shadow

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,7 +7,7 @@ export default styled.input`
 	padding:0 16px;
 
 	background: #FFF;
-	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
 
 	border: 2px solid transparent;
 	border-radius: 4px;
