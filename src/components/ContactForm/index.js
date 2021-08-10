59afecd8b91ea7e9733705b2ca9c34e046583720
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-09 22:58:45 -0300

    feat: remove special characters on submit form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -47,9 +47,9 @@ export default function ContactForm({ buttonLabel }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    // console.log({
-    //   name, email, phone, category,
-    // });
+    console.log({
+      name, email, phone: phone.replace(/\D/g, ''), category,
+    });
   }
 
   return (
