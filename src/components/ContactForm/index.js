98b513f8baa38781ef8ffa79e2c9205741e0ba38
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-09 22:56:52 -0300

    feat: create validations from phone input

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Form, ButtonContainer } from './styles';
 
 import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
 import useErrors from '../../hooks/useErrors';
 
 import FormGroup from '../FormGroup';
@@ -39,6 +40,10 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -72,7 +77,8 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           value={phone}
           placeholder="Telefone"
-          onChange={(event) => setPhone(event.target.value)}
+          onChange={handlePhoneChange}
+          maxLength="15"
         />
       </FormGroup>
 
